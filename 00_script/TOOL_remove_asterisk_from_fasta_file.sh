ca4620b5038afa8960de436b8f83a90ba8c40eab
furacca <chaca.davide@gmail.com>
2022-07-09 11:05:45 +0200

    Minor update

--- 00_script/TOOL_remove_asterisk_from_fasta_file.sh
+++ 00_script/TOOL_remove_asterisk_from_fasta_file.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 
+#Sweep the dust off
 
 echo ""
-echo "Attention!"
+echo "    ||                              "
+echo "    ||                              "
+echo "    ||                              "
+echo "    ||                              "
+echo "    ||                              "
+echo "    ||                              "
+echo "    ||     Sweeping the asterisk off"
+echo "    ||                              "
+echo "   /||\                             "
+echo "  /||||\                            "
+echo "  ======         __|__              "
+echo "  ||||||        / ~@~ \             "
+echo "  ||||||       |-------|            "
+echo "  ||||||       |_______|            "
 echo ""
-
-read -r -p "This operation will overwrite the original files. Are you sure that you want to proceed? [y/N] " response
+echo "The asterisks will be removed by all the following *.fasta file:"
+find .. -type f -name '*.fasta' -printf "   - %f\n"
+echo ""
+echo "This operation will OVERWRITE the original files."
+read -r -p "Are you sure that you want to proceed? [y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY])
         echo ""
-        find .. -type f -name '*.fasta' -exec sed -i 's/*//g' {} \;
-        echo "Well, now it's done. Nice choice."
-        echo "Have a beutiful day!"
-        echo ""
+        find .. -type f -name "*.fasta" -exec sed -i 's/*//g' {} ';'
+        echo "Done."
         ;;
     *)
-        echo "no"
+        echo "Nothing has be done."
+        echo ""
         ;;
 esac
+
+echo""
+echo "Have a beutiful day!"
+echo ""
