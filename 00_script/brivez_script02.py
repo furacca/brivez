f7ff63af21472269b06649e81117dab728d58272
furacca <chaca.davide@gmail.com>
2023-02-19 16:31:00 +0100

    PEP8

--- 00_script/brivez_script02.py
+++ 00_script/brivez_script02.py
@@ -21,7 +21,7 @@ from Bio import SeqIO
 def print_message(outputfilenumber):
 	print(f"Created -------->    {outputfilenumber}")
 
-	
+
 # --------------------------------------------------------------
 # Defining the output files' name
 
@@ -31,7 +31,7 @@ file_06 = "06_sequences_which_have_domains.fasta"
 file_07 = "07_extracted_domains.fasta"
 file_08 = "08_extracted_domains.csv"
 file_09 = "09_REPORT"
-	
+
 # --------------------------------------------------------------
 # Output's path
 output_path = "./tmp_output"
@@ -42,7 +42,7 @@ datafilename = ""
 for everyfile in os.listdir("./"):
     if fnmatch.fnmatch(everyfile, "ROTFLMAO*"):
         datafilename = everyfile
-fastasource = datafilename.replace("ROTFLMAO","")
+fastasource = datafilename.replace("ROTFLMAO", "")
 
 # --------------------------------------------------------------
 # Reading and parsing the hmmsearch output file
@@ -76,7 +76,8 @@ df = pd.read_csv(f"{output_path}/04_hmmsearch_output_table_data_parsed")
 print_message(file_04)
 
 # Filtering for the columns of interest
-df_selected = df[["target_name", "query_name", "accession1", "START_ali", "END_ali"]]
+df_selected = df[["target_name", "query_name",
+    "accession1", "START_ali", "END_ali"]]
 
 # Saving the filtered columns in tsv file
 df_selected.to_csv(f"{output_path}/05_pandas_sequences_table.tsv", sep="\t")
@@ -89,7 +90,6 @@ dict_seq = {}
 
 # dict_seq counter
 counter = 0
-
 
 
 with open(f"{output_path}/05_pandas_sequences_table.tsv", "r") as file_tsv:
@@ -107,7 +107,7 @@ with open(f"{output_path}/05_pandas_sequences_table.tsv", "r") as file_tsv:
             counter += 1
 
 
-#  dict_seq structure 
+#  dict_seq structure
 # {
 # 0: ['sp|P04275|VWF_HUMAN', 'vwA_MSA', 'empty', '1259', '1463'],
 # 1: ['sp|P04275|VWF_HUMAN', 'vwA_MSA', 'empty', '1485', '1657'],
@@ -128,30 +128,27 @@ with open(fastasource, "r") as file_fastasource:
     for everysequence in SeqIO.parse(file_fastasource, "fasta"):
         list_of_names_of_all_sequences.append(everysequence.id)
 
-# Looking for the length of the shortest sequence 
+# Looking for the length of the shortest sequence
 # If you have just one sequence, you can't determine which is the root (you need two sequences or more)
 
 
 # Set the chars need to be removed to zero
 char_to_be_removed_counter = 0
-    
+
 if len(list_of_names_of_all_sequences) > 1:
 
     shortest_sequence_lenght = 1000
 
     for eachname in list_of_names_of_all_sequences:
         if len(eachname) < shortest_sequence_lenght:
-            shortest_sequence_lenght = len(eachname)    
-    
-
-    
+            shortest_sequence_lenght = len(eachname)
 
     # x = position (in the common root) of each char
     for x in range(0, shortest_sequence_lenght):
-        
+
         # List of char found in the range; if len(char_check) > 1 then there are two different char for that position -> this is where the common root ends
         char_check = []
-        
+
         for every_sequence_name in list_of_names_of_all_sequences:
             if every_sequence_name[x] in char_check:
                 pass
@@ -160,8 +157,8 @@ if len(list_of_names_of_all_sequences) > 1:
 
         if len(char_check) == 1:
             char_to_be_removed_counter += 1
-    
-    
+
+
 # --------------------------------------------------------------
 # Creating the output file 06_sequence_with_domains.fasta
 # It contains the protein target (the one with SP if deepsig has been used, otherwise all protein inside the fasta source) and they sequence extracted
@@ -185,11 +182,12 @@ with open(f"{output_path}/06_sequences_which_have_domains.fasta", "a") as extrac
         if dict_seq[everyelement][0] in seq_already_written:
             pass
         else:
-            extraction_part2.write(f">{root}_{dict_seq[everyelement][0][char_to_be_removed_counter:]}\n{dict_seq[everyelement][5]}\n\n")
-		#>Hsapiens_VWF_Proteinasuper
-		#RECLCGALASYAAACAGRGVRVAWREPGRC.....
+            extraction_part2.write(
+                f">{root}_{dict_seq[everyelement][0][char_to_be_removed_counter:]}\n{dict_seq[everyelement][5]}\n\n")
+		# >Hsapiens_VWF_Proteinasuper
+		# RECLCGALASYAAACAGRGVRVAWREPGRC.....
             seq_already_written.append(dict_seq[everyelement][0])
-            
+
 print_message(file_06)
 
 # --------------------------------------------------------------
@@ -200,7 +198,8 @@ with open(f"{output_path}/07_extracted_domains.fasta", "a") as extraction_part3:
 
     for everyelement in dict_seq:
         domain = str(dict_seq[everyelement][5])
-        extraction_part3.write(f">{root}_{dict_seq[everyelement][0][char_to_be_removed_counter:]}-{dict_seq[everyelement][1]}-from{dict_seq[everyelement][3]}to{dict_seq[everyelement][4]}\n{domain[int(dict_seq[everyelement][3]):int(dict_seq[everyelement][4])]}\n")
+        extraction_part3.write(
+            f">{root}_{dict_seq[everyelement][0][char_to_be_removed_counter:]}-{dict_seq[everyelement][1]}-from{dict_seq[everyelement][3]}to{dict_seq[everyelement][4]}\n{domain[int(dict_seq[everyelement][3]):int(dict_seq[everyelement][4])]}\n")
 
 print_message(file_07)
 
@@ -209,13 +208,13 @@ print_message(file_07)
 # Extracing all the domains found inside the target in a single file
 
 with open(f"{output_path}/08_extracted_domains.csv", "a") as extraction_part4:
-	header_string = "Fasta file,sequence name,domain name,length,aa coords,sequence\n"
-	extraction_part4.write(header_string)
-			
-	for everyelement in dict_seq:
-        	domain = str(dict_seq[everyelement][5])
-            domain_length = int(dict_seq[everyelement][4]) - int(dict_seq[everyelement][3])
-        	extraction_part4.write(f"{root},{dict_seq[everyelement][0][char_to_be_removed_counter:]},{dict_seq[everyelement][1]},{domain_length},{dict_seq[everyelement][3]}-{dict_seq[everyelement][4]},{domain[int(dict_seq[everyelement][3]):int(dict_seq[everyelement][4])]}\n")
+    header_string = "Fasta file,sequence name,domain name,length,aa coords,sequence\n"
+    extraction_part4.write(header_string)
+
+    for everyelement in dict_seq:
+        domain = str(dict_seq[everyelement][5])
+        domain_length = int(dict_seq[everyelement][4]) - int(dict_seq[everyelement][3])
+        extraction_part4.write(f"{root},{dict_seq[everyelement][0][char_to_be_removed_counter:]},{dict_seq[everyelement][1]},{domain_length},{dict_seq[everyelement][3]}-{dict_seq[everyelement][4]},{domain[int(dict_seq[everyelement][3]):int(dict_seq[everyelement][4])]}\n")
 
 print_message(file_08)
 
@@ -225,12 +224,12 @@ print_message(file_08)
 # Combining all the domains found from ALL the target in a single fail
 
 with open("../tmp_folder/all_extracted_domains", "r") as newfile:
-	one_file = newfile.read().strip()
-	if one_file == "1":
-		with open("all_extracted_domains.fasta", "a") as commmonfile:
-			for everyelement in dict_seq:
-        			domain = str(dict_seq[everyelement][5])
-        			commmonfile.write(f">{root}_{dict_seq[everyelement][0][char_to_be_removed_counter:]}-{dict_seq[everyelement][1]}-from{dict_seq[everyelement][3]}to{dict_seq[everyelement][4]}\n{domain[int(dict_seq[everyelement][3]):int(dict_seq[everyelement][4])]}\n\n")
+    one_file = newfile.read().strip()
+    if one_file == "1":
+	    with open("all_extracted_domains.fasta", "a") as commmonfile: 
+            for everyelement in dict_seq:
+                domain = str(dict_seq[everyelement][5])
+                commmonfile.write(f">{root}_{dict_seq[everyelement][0][char_to_be_removed_counter:]}-{dict_seq[everyelement][1]}-from{dict_seq[everyelement][3]}to{dict_seq[everyelement][4]}\n{domain[int(dict_seq[everyelement][3]):int(dict_seq[everyelement][4])]}\n\n")
 
 
 # --------------------------------------------------------------
